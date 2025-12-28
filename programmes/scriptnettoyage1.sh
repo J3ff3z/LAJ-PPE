b015f5f5d2a629210cb79ed3b16eac85d05b665c
Laura Klima <lauraklima@MacBook-Air-de-Laura.local>
2025-12-29 00:23:21 +0100

    ajout nv trucs à supprimer et corrections erreurs

--- programmes/scriptnettoyage1.sh
+++ programmes/scriptnettoyage1.sh
@@ -8,17 +8,27 @@ numerotation=1
 
 signes="[+*#__--]"
 
-for fichier in dumps-text/*;
+for fichier in $(ls dumps-text | sort -t- -k2 -n);
 do
-    read -r url < "$fichier"
-    texte=$(cat "$fichier" | grep -viE "^\s*$signes|$signes.*$signes|BUTTON|IFRAME|Search|settings[[:space:]]+icon|share[[:space:]]+icon|Reklama|logo|Resources|Home|Menu|Strona[[:space:]]+główna|go!|^[-— =+]+$")
-    texte=$(echo "$texte" | sed -E '
-        s|https?://[^[:space:]]+||g
-        s|(^\|[[:space:]])[a-zA-Z0-9-]+(\.[a-z0-9]+)+||g
-        s|[A-Za-z0-9._]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}||g
-        s|\+?[1-9][0-9]{1,3}([[:space:].-]?[0-9]){6,12}||g
-        ')
-    echo "$texte"> "$dossiertest/lien-$numerotation.txt"
-    sleep 1
+    echo
+    grep -viE "^\s*$signes|$signes.*$signes|BUTTON|IFRAME|Search|settings[[:space:]]+icon|share[[:space:]]+icon|Refresh|Reklama|logo|Resources|Home|Menu|Strona[[:space:]]+główna|go!|^[-— =+]+$" "dumps-text/$fichier" \
+    | sed -E '
+        s|https?://[^[:space:]]+||g;
+        s|[a-zA-Z0-9-]+(\.[a-zA-Z0-9]+)+||g;
+        s|[A-Za-z0-9._]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}||g;
+        s|\+?[1-9][0-9]{1,3}([[:space:].-]?[0-9]){6,12}||g;
+        s|#?oEmbed[[:space:]]*\(JSON\)||g;
+        s|oEmbed[[:space:]]*\(XML\)||g;
+        s|#alternate[[:space:]]*alternate||g;
+        s|#alternate||g;
+        s|#?JSON||g;
+        s|\[INS\:[[:space:]]*\:INS\]||g;
+        s|#RSS[[:space:]]*RSS||g;
+        s|#[[:space:]]*RSS||g;
+        s|\[tr\?.*\]||g;
+        s|(pre-meta)||g;
+        s|\[[[:space:]]*\]||g;
+        s|\[X\]||g;
+    '> "$dossiertest/propre-$numerotation.txt"
     ((numerotation++))
 done
