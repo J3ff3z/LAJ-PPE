ae9f860669fd9f2c847b3f0e74745c8b083b077e
Jeffez <contact@jetruccharasch.fr>
2026-01-02 22:59:29 +0100

    Adding concordancer extractor to the script. Thanks for the regex Laura

--- programmes/comptage_occurence_script.sh
+++ programmes/comptage_occurence_script.sh
@@ -8,8 +8,39 @@ fi
 
 fichier="$1"
 
-resu=$(grep -oiE '\bgrzech(u|owi|em|y|om|ami|ach|ów)?\b|\b(péché)s?\b' "$fichier" | wc -l)
-echo "Occurence du mot : $resu"
+SORTIE="concordances/$(basename "$1").html"
+
+pattern='\b((grzech(u|owi|em|y|om|ami|ach|ów)?)|(péché)s?)|(大?罪)\b'
+sentences=$(sed 's/[.!?。…！？«»「」]/\n/g' "$fichier")
+
+count=0
+
+echo -e "<table class=\"table is-hoverable\">
+            <thead>
+                <tr>
+                    <th>Avant</th>
+                    <th>Mot</th>
+                    <th>Apres</th>
+                </tr>
+            </thead>" >> "$SORTIE"
+
+while IFS= read -r line; do
+    reste="$line"
+    while echo "$reste" | grep -qiE "$pattern"; do
+        count=$((count + 1))
+        word=$(echo "$reste" | grep -oiE "$pattern" | head -n 1)
+        before=$(echo "$reste" | sed -E "s/($word).*//I")
+        after=$(echo "$reste" | sed -E "s/^.*$before$word(.*)/\1/I")
+
+        echo -e "<tr>
+                    <td>$before</td>
+                    <td>$word</td>
+                    <td>$after</td>
+                </tr>" >> "$SORTIE"
+        
+        reste="$after"
+    done
+done <<< "$sentences"
 
 
-#.//ce_script dossiertextespropres/NOMFICHIERPROPRE(propre-1.txt etc)
+echo $count
